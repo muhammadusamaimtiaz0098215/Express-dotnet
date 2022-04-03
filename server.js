77ec978bacbfec7946942477694cdb243ed6443a
usama.imtiaz@virtual-force.com <usama.imtiaz@virtual-force.com>
2022-04-03 13:20:54 +0500

    mongodb connected with authentication

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 // const logger = require("./middleware/logger");
 
 const morgan = require("morgan");
 
-const bootcamps = require("./routes/bootcamps.js");
-
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const bootcamps = require("./routes/bootcamps.js");
 
 const app = express();
 
@@ -20,4 +23,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running on ${PORT}`));
+const server = app.listen(PORT, console.log(`Server is running on ${PORT}`));
+
+//Handle Unhandled Rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`ERROR: ${err.message}`);
+
+  //Close Server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
